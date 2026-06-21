0ab002039ca3d95029990d6c6d83f6b0d800541b
alphabit-technology <qubitcore.io@gmail.com>
2026-06-21 14:12:33 -0400

    Update to version 1.1.108

---
+++ modules/couple/pages/couple-galery/client/couple-galery-page.jsx
@@ -0,0 +1,10 @@
+
+'use strict';
+
+import PageContext from '@context/page-context';
+
+export default class CoupleGaleryPage extends PageContext {
+    constructor(props){
+        super(props);
+    }
+}
